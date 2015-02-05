ae342c110375c4738e90e5da15c11961815ed52a
Jim Eldred <gscjim@gmail.com>
2015-02-05 07:51:24 -0600

    explosion direction change

--- Assets/TimedExplosion.cs
+++ Assets/TimedExplosion.cs
@@ -20,7 +20,7 @@ public class TimedExplosion : MonoBehaviour {
 	void FixedUpdate () {
 		time_counter += Time.deltaTime;
 		if (time_counter > time_til_explosion && count < number ){
-			Vector2 force = new Vector2 (Random.Range(-2,2) * Power,Random.Range(-2,2)* Power);
+			Vector2 force = new Vector2 (Random.Range(-2,2) * Power,Random.Range(0,2)* Power);
 			Vector2 position = new Vector2 (rigidbody2D.position.x,rigidbody2D.position.y);
 			Vector3 explosion_start = new Vector3 (transform.position.x - 2, transform.position.y);
 			Quaternion rotation = new Quaternion (0, 0, 0, 0);
