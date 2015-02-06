d3a67d3ea9af759e25f01d4511b870f89efc8a96
Jim Eldred <gscjim@gmail.com>
2015-02-06 12:54:08 -0600

    hitpoints

---
+++ Assets/HealthPool.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPool : MonoBehaviour {
+	public int HitPoints;
+	public TextMesh HPDisplay;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		HPDisplay.text = HitPoints.ToString();
+
+		if (HitPoints <= 0) {
+			DestroyObject (gameObject);
+			}
+	
+	}
+}
