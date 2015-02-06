9e4e4aa5cc545fe8847117ba16b244e5611c4ae7
Jim Eldred <gscjim@gmail.com>
2015-02-06 06:10:12 -0600

    timer and scene changer

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+	float time_counter;
+	public GUIText timer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		time_counter += Time.deltaTime;
+		timer.text = time_counter.ToString();
+
+		if (time_counter > 100) {
+						Application.LoadLevel ("monkey");
+				}
+	}
+}
