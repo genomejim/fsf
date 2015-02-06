f667529b9f96038614f1094ac9fcf758ec83e1bf
Jim Eldred <gscjim@gmail.com>
2015-02-05 21:42:54 -0600

    spawning on alpha1

--- Assets/FlyingMovement.cs
+++ Assets/FlyingMovement.cs
@@ -14,23 +14,26 @@ public class FlyingMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+		Debug.Log (rigidbody2D.velocity);
+
 		if (rigidbody2D.position.x > right_patrol_boundary) {
 			Debug.Log (x_speed);
-						x_speed = -x_speed;
-						y_speed = -y_speed;
+
 						Flip ();
 		} else if (rigidbody2D.position.x < left_patrol_boundary) {
-						x_speed = -x_speed;
-						y_speed = -y_speed;
+
 						Flip ();
 				}
-		Debug.Log (rigidbody2D.velocity);
 		rigidbody2D.velocity = new Vector2 (x_speed, y_speed);
+
+
 	}
 	void Flip () {
 		//facing_right = !facing_right;
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
+		x_speed = -x_speed;
+		y_speed = -y_speed + Random.Range(-1,1);
 	}
 }
--- Assets/SpawnPrefab.cs
+++ Assets/SpawnPrefab.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class SpawnPrefab : MonoBehaviour {
 
-	public GameObject Prefab; 
+	public GameObject Prefab1;
+	public GameObject Prefab2;
 	public float time_between_spawns;
 	float time_counter;
 	public int total_to_spawn;
 	int total_spawned;
-	public Vector3 spawnpoint;
+	public Vector3 spawnpoint_one;
+	public Vector3 spawnpoint_two;
 	// Use this for initialization
 	void Start () {
 	
@@ -16,11 +18,22 @@ public class SpawnPrefab : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		//spawnpoint = new Vector3(100,15,0);
 		Quaternion rotation = new Quaternion (0, 0, 0, 0);
+		bool spawn_one = Input.GetKey (KeyCode.Alpha1);
+		bool spawn_two = Input.GetKey (KeyCode.Alpha2);
+		if (spawn_one && time_counter > time_between_spawns) {
+			Instantiate (Prefab1, spawnpoint_one, rotation);
+			time_counter = 0;
+				}
+		else if (spawn_two & time_counter > time_between_spawns) {
+			Instantiate (Prefab2, spawnpoint_two, rotation);
+			time_counter = 0;
+		}
 		time_counter += Time.deltaTime;
+
+		//auto spawn if total_to_spawn > 0
 		if (time_counter > time_between_spawns && total_spawned < total_to_spawn ){
-			Instantiate (Prefab, spawnpoint, rotation);
+			Instantiate (Prefab1, spawnpoint_one, rotation);
 			time_counter = 0;
 			total_spawned++;
 		}
