d543701b1998a1d62be06a097d424e8743474333
Jim Eldred <gscjim@gmail.com>
2015-02-13 06:32:33 -0600

    conditional animation

---
+++ Assets/jimanimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jimanimation : MonoBehaviour {
+	public float HorizontalMovement;
+	public Animator anim; // Refrerence to the animator
+	GameObject player;
+	// Use this for initialization
+	void Start () {
+		//player = gameObject.FindWithTag("Player");
+		player = GameObject.FindWithTag("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		//player = gameObject.Find ("animated player");
+		HorizontalMovement = Mathf.Abs(player.rigidbody2D.velocity.x);
+		anim.SetFloat ("Horizontal Movement", HorizontalMovement);
+
+	
+	}
+}
