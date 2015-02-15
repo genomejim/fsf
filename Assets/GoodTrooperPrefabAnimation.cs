b0b32e96b4cc365242f139017c61a479184cc550
Jim Eldred <gscjim@gmail.com>
2015-02-15 11:19:03 -0600

    issue 18, conditional animation for good troopers

---
+++ Assets/GoodTrooperPrefabAnimation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoodTrooperPrefabAnimation : MonoBehaviour {
+	public float HorizontalMovement;
+	public Animator anim; // Refrerence to the animator
+	GameObject trooper;
+	// Use this for initialization
+	void Start () {
+		//player = gameObject.FindWithTag("Player");
+		trooper = GameObject.FindWithTag("goodtrooper");
+		anim.SetFloat ("Horizontal Movement", 0);
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		//player = gameObject.Find ("animated player");
+		HorizontalMovement = Mathf.Abs(trooper.rigidbody2D.velocity.x);
+		anim.SetFloat ("Horizontal Movement", HorizontalMovement);
+		
+		
+	}
+}
